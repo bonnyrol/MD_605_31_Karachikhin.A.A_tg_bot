1912ef069635bed34c74f1b902cd68d544630417
bonnyrol <Artemkar1032@gmail.com>
2025-12-27 11:48:44 +0500

    project init

---
+++ src/tg_bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using dotenv.net;
+
+namespace tg_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DotEnv.Load();
+            var envVars = DotEnv.Read();
+
+            Console.WriteLine("Hello, World! " + envVars["TG_TOKEN"]);
+        }
+    }
+}
